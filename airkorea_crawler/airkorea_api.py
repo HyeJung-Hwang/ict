d02d0534c0a5daab1d8d039c4a6eb8aecc69bfa0
Hyejung Hwang <seathat33@gmail.com>
2023-08-08 12:42:09 +0900

    feat: response parsing 모듈

--- airkorea_crawler/airkorea_api.py
+++ airkorea_crawler/airkorea_api.py
@@ -1,6 +1,9 @@
 import requests
 import os
+import json
 from requests import Response
+from utils import convert_dt, get_hour
+import datetime
 
 def request_airkorea_api(station_name: str,page_no: int,data_term: str = "MONTH") -> Response :
     response = requests.get(
@@ -13,3 +16,24 @@ def request_airkorea_api(station_name: str,page_no: int,data_term: str = "MONTH"
         f"&dataTerm={data_term}"
     )
     return response
+
+def parse_airdata(content: Response.content) -> list:
+    result = []
+    airdata = json.loads(content)["response"]["body"]["items"]
+    for data in airdata:
+        temp_dt = data["dataTime"]
+        hour = get_hour(temp_dt)
+        if hour == "24":
+            dt = convert_dt(temp_dt)
+        else:
+            dt = datetime.datetime.strptime(temp_dt,"%Y-%m-%d %H:%M")
+        result.append({
+            "event_time": int(dt.timestamp()),
+            "pm_10": data["pm10Value"],
+            "o3": data["o3Value"], # 있을 수도 있고 없을 수도 있다
+            "no2": data["no2Value"],
+            "co": data["coValue"],
+            "so2": data["so2Value"],
+        })
+
+    return result
---
+++ utils.py
@@ -0,0 +1,15 @@
+import datetime
+import re
+
+def get_hour(dt: str) -> str:
+    pattern = "\d{4}-\d{2}-\d{2} (\d{2}):\d{2}"
+    result = re.findall(pattern,dt)
+    return result[-1]
+
+def convert_dt(dt: str) -> datetime.datetime:
+    dt_format = "%Y-%m-%d %H:%M"
+    pattern = "(\d{4}-\d{2}-\d{2}) \d{2}(:\d{2})"
+    prev_dt = re.sub(pattern,r"\1 23\2",dt)
+    result =  datetime.datetime.strptime(prev_dt,dt_format) \
+        + datetime.timedelta(hours=1)
+    return result

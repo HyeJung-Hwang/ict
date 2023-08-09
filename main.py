bb4f879c220afe6ffb7bfb371f80d85795411fc5
Hyejung Hwang <seathat33@gmail.com>
2023-08-09 09:37:32 +0900

    refactor : run() 사용

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import dotenv
 import json
 import pprint
 from airkorea_crawler import airkorea_api
-if __name__ == "__main__":
+
+
+def run_extract():
+    # ETL 중 Extract
     dotenv.load_dotenv()
 
     response = airkorea_api.request_airkorea_api(
-        station_name="마포구",
-        page_no=1,
-        data_term="MONTH"
+        station_name="마포구", page_no=1, data_term="MONTH"
     )
     print(response)
     if response.status_code != 200:
-        #  json.dumps(response)
-        exit(0) # early return
-    else:
-        parsed_airdata = airkorea_api.parse_airdata(response.content)
+        return json.dumps(response)
+    parsed_airdata = airkorea_api.parse_airdata(response.content)
     print(parsed_airdata)
+
+
+if __name__ == "__main__":
+    run_extract()
